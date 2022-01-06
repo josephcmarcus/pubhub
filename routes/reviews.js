697eb762e09dc423311a93d8ceb625d336b086f6
josephcmarcus <83798101+josephcmarcus@users.noreply.github.com>
2022-01-05 19:43:22 -0500

    commit twelve

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-const Pub = require('../models/pub');
-const Review = require('../models/review');
 const reviews = require('../controllers/reviews');
 const { reviewSchema } = require('../schemas.js');
 const { validateReview, isLoggedIn, isReviewAuthor } = require('../utils/middleware')
@@ -11,12 +9,6 @@ const ExpressError = require('../utils/ExpressError');
 
 router.post('/', validateReview, isLoggedIn, catchAsync(reviews.createReview));
 
-router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(async(req, res) => {
-  const { id, reviewId } = req.params;
-  const pub = await Pub.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-  await Review.findByIdAndDelete(reviewId);
-  req.flash('success', 'Successfully deleted review.');
-  res.redirect(`/pubs/${id}`);
-}))
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview))
 
 module.exports = router;
